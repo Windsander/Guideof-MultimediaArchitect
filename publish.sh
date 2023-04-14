99001967ba380ffe8722308add864b402af088f7
Arikan Li <panlina68@hotmail.com>
2023-04-14 21:30:36 +0800

    fix:    publish script

--- publish.sh
+++ publish.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env sh
 
+if [ "$1" != "" ]; then
+    echo "正在发布分支: " $1
+else
+    echo "需要指定发布分支"
+    exit
+fi
+
 echo '开始执行 publish 命令'
 # 生成静态文件
 echo '执行命令: gitbook build .'
-gitbook install
-gitbook build
+# gitbook install
+# gitbook build
 
 # 进入生成的文件夹
 echo "执行命令: cd ./_book\n"
@@ -14,7 +21,7 @@ echo "发布开始: start\n"
 git init
 git add -A
 git commit -m 'release:    publish last version!'
-git push -f https://github.com/Windsander/Project_M.git main:publish
+git push -f git@github.com:Windsander/Project_M.git $1:publish
 echo "发布完成: https://arikanli.github.io/<REPO>"
 
 # 返回到上一次的工作目录
