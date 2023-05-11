dcf14dbf0c2a2567035c432014b75091e8b7ee6d
Arikan Li <panlina68@hotmail.com>
2023-05-11 21:55:52 +0800

    feat:    provide bilateral filter WebGL example

--- Chapter_1/Examples/Playground/testprocess.js
+++ Chapter_1/Examples/Playground/testprocess.js
@@ -6,12 +6,25 @@
  * provide effects based on Chapter_1 examples
  */
 
+/*================================== Prepare ==================================*/
+/**
+ * Calculate single pixel bias for kernel offset
+ *
+ * @param width Input Texture(Screen) Width
+ * @param height Input Texture(Screen) Height
+ */
 function pixel_bias(width, height) {
     return new Float32Array([
         1.0 / width, 1.0 / height
     ]);
 }
 
+/**
+ * Calculate gaussian kernel for wight matrix
+ *
+ * @param step Convolution Kernel step
+ * @param delta Gaussian Distribution Factor
+ */
 function calculate_gaussian_kernel(step, delta) {
     let n = step * 2 + 1;
     let kernel = new Float32Array(n * n);
@@ -32,6 +45,35 @@ function calculate_gaussian_kernel(step, delta) {
     return kernel;
 }
 
+/**
+ * Calculate gaussian kernel for wight matrix
+ *
+ * @param step Convolution Kernel step
+ * @param delta_d Gaussian Distribution Factor
+ */
+function calculate_bilateral_kernel(step, delta_d, delta_r) {
+    let n = step * 2 + 1;
+    let kernel = new Float32Array(n * n);
+    let factor_1 = 1.0 / (Math.sqrt(2.0 * Math.PI) * delta_d * delta_r);
+    let factor_d = 1.0 / (2.0 * delta_d * delta_d);
+    let factor_r = 1.0 / (2.0 * delta_r * delta_r);
+
+    let normalize_div = 0;
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            let diff = Math.pow(i - step, 2) + Math.pow(j - step, 2);
+            kernel[j + n * i] = factor_1 * Math.exp(-diff * factor_d);
+            normalize_div += kernel[i];
+        }
+    }
+    for (let i = 0; i < kernel.length; i++) {
+        kernel[i] /= normalize_div;
+    }
+    return kernel;
+}
+
+/*================================== Process ==================================*/
+
 class TestProcess {
     #driver;
     #source_buffer;
@@ -41,10 +83,11 @@ class TestProcess {
     #positions;
     #pixel_bias;
     #gaussian_kernel;
+    #bilateral_range;
 
     #effect_gaussian_norm;
     #effect_gaussian_fast;
-    #effect_bilateral;
+    #effect_bilateral_norm;
     #effect_sobel;
     #effect_NMS;
 
@@ -62,9 +105,9 @@ class TestProcess {
             +1.0, +1.0, 0.0, //1.0, 1.0
         ]);
 
-        this.#effect_gaussian_norm = utils.addShaderProg(driver, 'filter_common_vs', 'filter_gaussian_ps');
+        this.#effect_gaussian_norm = utils.addShaderProg(driver, 'filter_common_vs', 'filter_gaussian_norm_ps');
         this.#effect_gaussian_fast = utils.addShaderProg(driver, 'filter_common_vs', 'filter_gaussian_fast_ps');
-        // this.#effect_bilateral = utils.addShaderProg(driver, 'filter_common_vs', 'filter_bilateral_ps');
+        this.#effect_bilateral_norm = utils.addShaderProg(driver, 'filter_common_vs', 'filter_bilateral_norm_ps');
         // this.#effect_sobel = utils.addShaderProg(driver, 'filter_common_vs', 'filter_sobel_ps');
         // this.#effect_NMS = utils.addShaderProg(driver, 'filter_common_vs', 'filter_NMS_ps');
     }
@@ -83,6 +126,7 @@ class TestProcess {
 
     config(effect_params) {
         this.#gaussian_kernel = calculate_gaussian_kernel(effect_params.gaussian_step, effect_params.gaussian_delta);
+        this.#bilateral_range = effect_params.gaussian_range;
     }
 
     bind() {
@@ -115,7 +159,12 @@ class TestProcess {
                 break;
             }
             case 3: {
-
+                this.#effect_bilateral_norm.use();
+                this.#effect_bilateral_norm.setUniform("target_texture", this.#source_buffer.texture)
+                this.#effect_bilateral_norm.setUniform("gaussian_matrix", this.#gaussian_kernel)
+                this.#effect_bilateral_norm.setUniform("gaussian_range", this.#bilateral_range)
+                this.#effect_bilateral_norm.setUniform("pixel_bias", this.#pixel_bias)
+                this.#draw_effect(this.#effect_bilateral_norm);
                 break;
             }
             case 4: {
